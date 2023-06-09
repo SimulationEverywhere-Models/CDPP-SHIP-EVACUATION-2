2538daa3c686b3e963667d7a328a175b820afb07
Srijan1972 <srijang@live.com>
2023-06-09 15:05:35 -0400

    Add files via upload

---
+++ shipevacuation_2.bat
@@ -0,0 +1,3 @@
+C:/eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe  -mshipevacuation_2MA.ma -oshipevacuation_2MA.out -lshipevacuation_2LOG.log 
+rem 
+pause
---
+++ shipevacuation_2DRW.bat
@@ -0,0 +1,2 @@
+drawlog  -mshipevacuation_2MA.ma -ccubierta -lshipevacuation_2LOG.log -0 >shipevacuation_2.drw
+pause
